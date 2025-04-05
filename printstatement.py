f1cb5abd9593f59d2b4cfc68607bb7d6a8e7fcf7
Prayaan1711 <spyfyi1711@gmail.com>
2025-04-05 09:32:54 +0530

    123py

---
+++ printstatement.py
@@ -0,0 +1,13 @@
+print("My name is prayaan")
+
+print(4)
+
+print("Messi \n")
+print("Ronaldo")
+
+print("Hello", 5)
+
+#End arguement of print statement
+print("welcome to", end = " ")
+
+print("Suiii")
