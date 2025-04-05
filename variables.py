b1e446fa06aeea32159c3791dc315d16e0bb8da2
Prayaan1711 <spyfyi1711@gmail.com>
2025-04-05 09:46:32 +0530

    123

---
+++ variables.py
@@ -0,0 +1,13 @@
+x = 5
+y = "Prayaan"
+print(x)
+print(y)
+
+codingal = "codingal"
+print(codingal)
+
+#User input
+name = input("Enter your name: ")
+place = input("Enter your city: ")
+
+print("Hello", name, " from ", place, " welcome.")
